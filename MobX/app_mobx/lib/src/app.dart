8c2902883501be9943f83e507236576940274e91
Natanael Candido <natanael@sistemaquantum.com.br>
2021-09-10 17:48:18 -0300

    improvements app.dart

--- MobX/app_mobx/lib/src/app.dart
+++ MobX/app_mobx/lib/src/app.dart
@@ -1,8 +1,17 @@
+import 'package:app_mobx/src/shared/router_app.dart';
+import 'package:app_mobx/src/shared/styles/theme_app.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeApp().getThemeData,
+      initialRoute: RouterApp.HOME_KEY,
+      routes: {
+        RouterApp.HOME_KEY: (_) => RouterApp.HOME_PAGE,
+      },
+    );
   }
 }
